cc0868e3cbdadb8d0eb31cb4c4c6dc86bff4f3b9
defendimattia <defendi.mattia92@gmail.com>
2025-04-10 16:06:47 +0200

    add: filter by title option

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react"
 import films from "./data/films"
+import { use } from "react"
 
 function App() {
 
   const [selectGenre, setSelectGenre] = useState("Tutti")
   const [filteredFilms, setFilteredFilms] = useState(films)
+  const [filterText, setFilterText] = useState("")
 
+
+  // genre event
   useEffect(() => {
     if (selectGenre === "Tutti") {
       setFilteredFilms(films)
@@ -14,6 +18,15 @@ function App() {
       setFilteredFilms(filteredFilms)
     }
   }, [selectGenre])
+
+
+  function submitFunction(event) {
+    event.preventDefault()
+    const filteredText = filteredFilms.filter(film => film.title.toLowerCase().includes(filterText.toLowerCase()))
+    setFilterText("")
+    setFilteredFilms(filteredText)
+  }
+
 
   return (
     <>
@@ -30,6 +43,13 @@ function App() {
         <option>Romantico</option>
         <option>Azione</option>
       </select>
+      <hr />
+      <h2>Filtro titolo</h2>
+      <form onSubmit={submitFunction}>
+        <input type="text" value={filterText} onChange={e => setFilterText(e.target.value)} />
+        <button type="submit">cerca</button>
+      </form>
+    {filterText}
     </>
   )
 }
