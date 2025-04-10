557dda13e2304f1283a1de2505e8844fdd64b2d5
defendimattia <defendi.mattia92@gmail.com>
2025-04-10 14:50:54 +0200

    add: select and change language

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,17 @@ function App() {
     <>
       <h1>Films</h1>
       <ul>
-        {films.map((film, i) => <li key={i}><p>title : {film.title} genre: {film.genre}</p></li>)}
+        {films.map((film, i) => <li key={i}><p>titolo : {film.title} genere: {film.genre}</p></li>)}
       </ul>
+      <hr />
+      <h2>Filtro genere</h2>
+      <select>        
+        <option>Tutti</option>
+        <option>Fantascienza</option>
+        <option>Thriller</option>
+        <option>Romantico</option>
+        <option>Azione</option>
+        </select>
     </>
   )
 }
