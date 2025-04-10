81e2eb8a7d64b06bac6d01bc8e85b8acff748d5f
defendimattia <defendi.mattia92@gmail.com>
2025-04-10 14:45:31 +0200

    add: films.js and logic to rappresent

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import films from "./data/films"
+
 function App() {
 
   return (
     <>
+      <h1>Films</h1>
+      <ul>
+        {films.map(film => <li><p>title : {film.title} genre: {film.genre}</p></li>)}
+      </ul>
     </>
   )
 }
